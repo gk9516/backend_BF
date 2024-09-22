785ee9df5b05dcb3d0447c9cc65b0990cd798e59
Ganesh K <103759838+gk9516@users.noreply.github.com>
2024-09-22 17:13:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+@app.route('/bfhl', methods=['POST'])
+def process_data():
+    try:
+        data = request.json.get('data')
+        if not data:
+            return jsonify({"is_success": False, "message": "No data provided"}), 400
+
+        user_id = "ganeshk_02042004"  
+        email = "ganeshrajan2.00@gmail.com"  
+        roll_number = "RA2111003010298"
+
+        numbers = [item for item in data if item.isdigit()]
+        alphabets = [item for item in data if item.isalpha()]
+        highest_alphabet = sorted(alphabets, key=lambda x: x.lower(), reverse=True)[:1]
+
+        return jsonify({
+            "is_success": True,
+            "user_id": user_id,
+            "email": email,
+            "roll_number": roll_number,
+            "numbers": numbers,
+            "alphabets": alphabets,
+            "highest_alphabet": highest_alphabet
+        })
+
+    except Exception as e:
+        return jsonify({"is_success": False, "message": str(e)}), 500
+
+@app.route('/bfhl', methods=['GET'])
+def get_operation_code():
+    return jsonify({"operation_code": 1})
+
+if __name__ == '__main__':
+    app.run(debug=True)
